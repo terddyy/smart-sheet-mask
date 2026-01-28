36018535505d272c72dcbb0050144ae37f4fd38b
terddyy <terddy03@gmail.com>
2026-01-29 02:23:18 +0800

    .

---
+++ android_backup/app/.cxx/Debug/2b6a25u1/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Terddy.LAPTOP-CVSRCLGL/.gradle/caches/8.14.3/transforms/e5d19bd6b3826afe5eb577927188e196/transformed/hermes-android-0.81.5-debug/prefab/modules/libhermes/libs/android.x86/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Terddy.LAPTOP-CVSRCLGL/.gradle/caches/8.14.3/transforms/e5d19bd6b3826afe5eb577927188e196/transformed/hermes-android-0.81.5-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
---
+++ android_backup/app/.cxx/Debug/2b6a25u1/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "C:/Users/Terddy.LAPTOP-CVSRCLGL/Desktop/smart-sheet-mask/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/493d2d3n/obj/x86_64/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Terddy.LAPTOP-CVSRCLGL/Desktop/smart-sheet-mask/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
